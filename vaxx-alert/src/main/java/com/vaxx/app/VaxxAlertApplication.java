21556ed3bfa1dd084269d2ff5e30a77512cf16c9
sarav <saravananv123@outlook.com>
2021-05-13 02:50:10 +0530

    Vaxx-alert init commit

---
+++ vaxx-alert/src/main/java/com/vaxx/app/VaxxAlertApplication.java
@@ -0,0 +1,13 @@
+package com.vaxx.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaxxAlertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaxxAlertApplication.class, args);
+	}
+
+}
---
+++ vaxx-alert/src/test/java/com/vaxx/app/VaxxAlertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaxx.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaxxAlertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
